63b6b0d00fab7f6f4fc2e5c9b2c0cb9d42a657a3
Andrey <deucefield@gmail.com>
2022-10-02 18:43:31 +0300

    homework #4 and #5 has done

--- src/main/java/core/lesson5/MainApp.java
+++ src/main/java/core/lesson5/MainApp.java
@@ -1,31 +1,24 @@
 package core.lesson5;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainApp {
-    public static final String filePath = "src/main/java/resources/data.csv";
+    public static final String filePath = "src/main/resources/data.csv";
     public static File csvFile = new File(filePath);
-    AppData appData = new AppData();
+    public static String[] header = {"V1;", "V2;", "V3;", "V4;", "V5;\n"};
+    public static int[][] data = {{1,2,3,4,5},{6,7,8,9,10}};
 
     public static void main(String[] args) throws IOException {
+        writeToFile();
 
-        String[] header = {"V1;", "V2;", "V3;", "V4;", "V5;\n"};
-        int[][] data = {{1,2,3,4,5},{6,7,8,9,10}};
-
-
-        writeToFile(csvFile, header, data);
-
-
+        readFile();
     }
 
-    public static void writeToFile(File file, String[] header, int[][] data) throws IOException {
-
-
-        FileOutputStream writer = new FileOutputStream(file, true);
+    public static void writeToFile() throws IOException {
+        FileOutputStream writer = new FileOutputStream(csvFile);
         for(String s: header) {writer.write(s.getBytes());}
         String result = "";
         for(int[] row: data) {
@@ -35,6 +28,37 @@ public class MainApp {
             result+="\n";
         }
         writer.write(result.getBytes());
+        writer.close();
     }
 
+    public static AppData readFile() throws IOException {
+        AppData appData = new AppData();
+        FileReader fr = new FileReader(csvFile);
+        BufferedReader reader = new BufferedReader(fr);
+
+        List<List<String>> records = new ArrayList<>();
+
+        String header = reader.readLine();
+        appData.setHeader(header.split(";"));
+
+
+        while ((header = reader.readLine()) != null) {
+            String[] values = header.split(";");
+            records.add(Arrays.asList(values));
+        }
+
+        int[][] resultData = new int[records.size()][5];
+
+        for(int i=0;i<records.size();i++){
+            for(int j=0;j<records.get(i).size();j++){
+                resultData[i][j] = Integer.valueOf(records.get(i).get(j));
+            }
+        }
+        appData.setData(resultData);
+
+        /*String data = reader.readLine(true);*/
+
+
+        return appData;
+    }
 }
